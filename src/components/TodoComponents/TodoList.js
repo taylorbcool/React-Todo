1ee89509174d70418b89b0b62d7fabf313ee4a06
Taylor Cool <taycool@gmail.com>
2019-12-02 16:10:55 -0800

    added p tag on todolist

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -5,7 +5,8 @@ const TodoList = props => {
 // console.log(props)
     return (
         <div className="todo-list">
-            <h3>To-do List:</h3>
+            <h3>To-do List</h3>
+            <p>Click an item to mark it as completed</p>
         {props.todoList.map(todo => (
             <Todo key={todo.id} todo={todo} />
         ))}
